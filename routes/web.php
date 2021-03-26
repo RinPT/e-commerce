c538f53e3c4fe07454075ef9783a98dbcadd8194
Erdagli574 <70316787+Erdagli574@users.noreply.github.com>
2021-03-26 22:03:38 +0200

    web routes modified

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,40 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserAddressController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //Home Page:
-Route::get('/','App\Http\Controllers\HomeController@index')-> name('home');
+
+Route::get('/', [HomeController::class, 'index'])-> name('home');
 
 //Authorization :
 
-Route::post('/logout','App\Http\Controllers\Auth\LogoutController@store')-> name('logout');
+Route::post('/logout', [LogoutController::class, 'store'])-> name('logout');
 
-Route::get('/login','App\Http\Controllers\Auth\LoginController@index') -> name('login') -> middleware('guest');;
-Route::post('/login','App\Http\Controllers\Auth\LoginController@store');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'store']);
 
-Route::get('/register','App\Http\Controllers\Auth\RegisterController@index') -> name('register') -> middleware('guest');
-Route::post('/register','App\Http\Controllers\Auth\RegisterController@store');
-
-//User :
-
-Route::get('/userprofile/{user}','App\Http\Controllers\UserController@index') -> name('user.profile'); //display userprofile
-
-Route::delete('userprofile/{user}','App\Http\Controllers\UserController@destroy') -> name('user.destroy'); //delete user
-Route::post('/userprofile/{user}/update','App\Http\Controllers\UserController@update_info')-> name('user.update'); //updates user info
-
-Route::post('/userprofile/{user}/passwordchange','App\Http\Controllers\UserController@update_password')-> name('user.password'); //updates user password
-
-//User Address Operations :
-
-Route::get('/userprofile/{user}/address','App\Http\Controllers\UserAddressController@index') -> name('user.address'); //display all saved addresses of this user
-
-Route::get('/userprofile/{user}/address/{user_address}','App\Http\Controllers\UserAddressController@display') -> name('address.display'); //display specific address update form
-
-Route::post('/userprofile/{user}/address/{user_address}','App\Http\Controllers\UserAddressController@update')-> name('address.update'); //updates specific address
-
-Route::delete('/userprofile/{user}/address/{user_address}','App\Http\Controllers\UserAddressController@destroy')-> name('address.destroy'); //deletes the specific address
-
-Route::get('/userprofile/{user}/newaddress','App\Http\Controllers\UserAddressController@show') -> name('address.add'); //shows a new address adding form
-
-Route::post('/userprofile/{user}/newaddress','App\Http\Controllers\UserAddressController@store') -> name('address.store'); //save the new added address
+Route::get('/register', [RegisterController::class, 'index']) -> name('register') -> middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
 
 
+Route::group(['prefix' => "/username"], function() {
+    //User :
+    Route::get('/{user:username}', [UserController::class, 'index']) -> name('user.profile'); //display userprofile
+    Route::delete('/{user:username}', [UserController::class, 'destroy']) -> name('user.destroy'); //delete user
+    Route::post('/{user:username}/update', [UserController::class, 'update_info'])-> name('user.update'); //updates user info
+    Route::post('/{user:username}/passwordchange', [UserController::class, 'update_password'])-> name('user.password'); //updates user password
 
+    //User Address Operations :
+    Route::get('/{user}/address', [UserAddressController::class, 'index']) -> name('user.address'); //display all saved addresses of this user
+    Route::get('/{user:username}/address/{user_address}', [UserAddressController::class, 'display']) -> name('address.display'); //display specific address update form
+    Route::post('/{user:username}/address/{user_address}', [UserAddressController::class, 'update'])-> name('address.update'); //updates specific address
+    Route::delete('/{user:username}/address/{user_address}', [UserAddressController::class, 'destroy'])-> name('address.destroy'); //deletes the specific address
+    Route::get('/{user:username}/newaddress', [UserAddressController::class, 'show']) -> name('address.add'); //shows a new address adding form
+    Route::post('/{user:username}/newaddress', [UserAddressController::class, 'store']) -> name('address.store'); //save the new added address
+});
