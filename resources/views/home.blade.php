bd74380b5c6d32af4b874cf059d8f3ed77f171fc
Erdagli574 <70316787+Erdagli574@users.noreply.github.com>
2021-03-26 23:40:56 +0200

    bootstrap added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-  <div class="flex justify-center">
-
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-    	<h> Home Page </h>
-    </div>
-    
-  </div>
+    <main class="mt-3">
+        <div class="d-flex justify-content-center">
+            <div class="w-8/12 bg-white p-6 rounded-lg">
+                <h> Home Page </h>
+            </div>
+          </div>
+    </main>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,55 +2,57 @@
 
 <html>
    <head>
-      <meta charset = "utf-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1">
-       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-       <link rel="stylesheet" type="text/css" href="{{  asset('css/app.css') }}">
-       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="{{  asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-      <title>E-Commerce</title>
+        <title>E-Commerce</title>
    </head>
 
    <body class="bg-gray-400">
-      <nav class="p-6 bg-white flex justify-between mb-6">
-         <ul class="flex items-center">
-            <li>
-               <a href="{{ route('home')}}" class="p-3"> Home </a>
-            </li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex align-items-center">
+            <a class="navbar-brand" href="#">Our Name</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-between mt-1" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="{{ route('home')}}" class="nav-link"> Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"> Products </a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    @auth
+                       <li class="nav-item">
+                          <a href="{{route('user.profile',auth()->user())}}" class="nav-link"> {{ auth()-> user() -> username }} </a>
+                       </li>
+                       <li class="nav-item">
+                          <form action="{{ route('logout')}}" method="post" class="nav-link">
+                             @csrf
+                                <button type="submit" > Logout </button>
 
-            <li>
-               <a href="#" class="p-3"> Products </a>
-            </li>
+                          </form>
 
-         </ul>
+                       </li>
+                    @endauth
 
-         <ul class="flex items-center">
-            @auth
-               <li>
-                  <a href="{{route('user.profile',auth()->user())}}" class="p-3"> {{ auth()-> user() -> username }} </a>
-               </li>
-               <li>
-                  <form action="{{ route('logout')}}" method="post" class="p-3 inline">
-                     @csrf
-                        <button type="submit" > Logout </button>
-
-                  </form>
-
-               </li>
-            @endauth
-
-            @guest
-               <li>
-                  <a href="{{ route('login')}}" class="p-3"> Login </a>
-               </li>
-               <li>
-                  <a href="{{ route('register')}}" class="p-3"> Register </a>
-               </li>
-            @endguest
-         </ul>
-
-      </nav>
-      @yield('content')
+                    @guest
+                       <li class="nav-item">
+                          <a href="{{ route('login')}}" class="nav-link"> Login </a>
+                       </li>
+                       <li class="nav-item">
+                          <a href="{{ route('register')}}" class="nav-link"> Register </a>
+                       </li>
+                    @endguest
+                 </ul>
+            </div>
+        </nav>
+        @yield('content')
    </body>
 </html>
