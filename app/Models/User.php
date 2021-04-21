db6e06a01f8453311c971dedc62e46ad41bd0f03
Berk ÇETİN <cetinn.berkk@gmail.com>
2021-04-21 21:40:51 +0300

    admin dashboard UI added

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,5 @@ class User extends Authenticatable
     public function addresses() {
         return $this->hasMany(UserAddress::class);
     }
+
 }
--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -28,4 +28,8 @@ class UserAddress extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function country() {
+        return $this->belongsTo(Countries::class);
+    }
 }
