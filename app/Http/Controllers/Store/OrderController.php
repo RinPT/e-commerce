9dcf69f7cdd326f6dccd616b8b874e8658f76a46
Erdagli574 <70316787+Erdagli574@users.noreply.github.com>
2021-06-18 22:18:53 +0300

    store order management started

--- app/Http/Controllers/Store/OrderController.php
+++ app/Http/Controllers/Store/OrderController.php
@@ -24,63 +24,29 @@ class OrderController extends Controller
         ]);
     }
 
-    public function create()
-    {
-        return view('store.orders.create', [
-            'store' => $this->logged_author,
-        ]);
-    }
-
-
-    public function store(Request $request)
-    {
-        try {
-            $order = Order::create([
-                'store_id' => $request->store_id,
-                'store_name' => $request->store_name,
-                'store_url'=> $request->store_url,
-                'user_id'=> $request->user_id,
-                'user_type'=> $request->store_id,
-                'name'=> $request->name,
-                'surname'=> $request->surname,
-                'username'=> $request->username,
-                'email'=> $request->email,
-                'gender'=> $request->gender,
-                'comment'=> $request->comment,
-                'total'=> $request->total,
-                'order_status'=> $request->order_status,
-                'currency_code'=> $request->currency_code,
-                'ip_address'=> $request->ip_address,
-                'user_agent'=> $request->user_agent,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-
-
-            ]);
-            return back()->with('success', 'New order added.');
-
-
-        } catch (\Exception $e) {
-            if ($e->getCode() == 23000) {
-
-                return back()->with('error', 'There is already an order for this name.');
-            }
-        }
-
-        return back();
-    }
-
     public function show_pending()
     {
-        return view('admin.order.index')->with([
-            'orders' => DB::select('select * from orders where order_status = ?', ['waiting'])
+        $orders = Order::where('store_id', '=', $this->logged_author->id)->where('order_status', '=', 'waiting')->get();
+
+        return view('store.orders.index', [
+            'orders' => $orders,
         ]);
     }
 
     public function show_canceled()
     {
-        return view('admin.order.index')->with([
-            'orders' => DB::select('select * from orders where order_status = ?', ['cancelled'])
+        $orders = Order::where('store_id', '=', $this->logged_author->id)->where('order_status', '=', 'waiting')->get();
+
+        return view('store.orders.index', [
+            'orders' => $orders,
+        ]);
+    }
+
+    public function show_cancel_request() {
+        $orders = Order::where('store_id', '=', $this->logged_author->id)->where('order_status', '=', 'cancell request')->get();
+
+        return view('store.orders.index', [
+            'orders' => $orders,
         ]);
     }
 
